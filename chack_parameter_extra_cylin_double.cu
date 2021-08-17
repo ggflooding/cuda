755b1710fbc956a2f59d7492a33e3997fab04a9a
ggflooding <34182283+ggflooding@users.noreply.github.com>
2021-08-17 18:21:52 +0800

    Update chack_parameter_extra_cylin_double.cu

--- chack_parameter_extra_cylin_double.cu
+++ chack_parameter_extra_cylin_double.cu
@@ -488,27 +488,8 @@ void OutWatch(int t, double err, double rhoav){
 	printf("Average Density = %.15lf\n", rhoav);
 }
 
-void Outp(double t){
-	FILE *fp;
-	char filename[0];
-	sprintf(filename,"%d%s", t ,".dat");
-	fp = fopen(filename, "w");
-	fprintf(fp, "Title=\"LBM Lid Driven Cavity\"\n");
-	fprintf(fp, "VARIABLES=\"X\",\"Y\",\"cylin\",\"U\",\"V\",\"P\",\"OMG\"\n");
-	fprintf(fp, "ZONE T=\"BOX\",I=%d,J=%d,F=POINT\n", NY, NX);
-	for (int i = 0; i < NX; i++){
-		for (int j = 0; j < NY; j++){
-			fprintf(fp, "%.15lf ", (double)i / (NY - 1) / DX_LB);
-			fprintf(fp, "%.15lf ", (double)j / (NY - 1) / DX_LB);
-			fprintf(fp, "%.15lf ", domain[Ord2(i, j, NX)].B);
-			fprintf(fp, "%.15lf ", domain[Ord2(i, j, NX)].u1[0]);
-			fprintf(fp, "%.15lf ", domain[Ord2(i, j, NX)].u1[1]);
-			fprintf(fp, "%.15lf ", domain[Ord2(i, j, NX)].rho);
-			fprintf(fp, "%.15lf\n", domain[Ord2(i, j, NX)].vor);
-		}
-	}
-	fclose(fp);
-	return;
+void Outp(int t){
+	
 }
 
 void Point_checkX_a(int t, double ux_a){
